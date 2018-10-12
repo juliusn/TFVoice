c23696d5edd126aa42fc3060ac3f261e9e41ffd2
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2018-10-12 14:29:57 +0300

    Don't store unsupported files. Clear storage.

--- public/javascripts/dropzone-audioupload-options.js
+++ public/javascripts/dropzone-audioupload-options.js
@@ -1,6 +1,5 @@
 Dropzone.options.waveUpload = {
-  paramName: 'waves',
-  parallelUploads: 10,
+  paramName: 'wave',
   maxFiles: null,
   maxFilesize: 128,
   acceptedFiles: 'audio/wav',
--- routes/upload.js
+++ routes/upload.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = new express.Router();
 const multer = require('multer');
-const upload = multer(
-    {dest: 'tmp/uploads/'}
-);
+const UPLOAD_PATH = 'tmp/uploads/';
+const path = require('path');
 
-router.post('/', upload.single('waves'), (req, res) => {
-  console.log('uploaded ', req.file);
-  return res.status(200).send();
+const upload = multer(
+    {
+      dest: UPLOAD_PATH,
+      fileFilter: function(req, file, cb) {
+        const filetypes = /wave|wav/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(
+            path.extname(file.originalname).toLowerCase());
+        cb(null, (mimetype && extname));
+      },
+    }
+);
+const fs = require('fs');
+const {promisify} = require('util');
+const clear = promisify(fs.unlink);
+
+router.post('/', upload.single('wave'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+    return res.status(415).send('Only audio/wav files are supported.');
+  }
+  clear(file.path);
+  return res.sendStatus(200);
 });
 
 module.exports = router;
