b8aff83a0a6ff260cebad2fa28219400c6ffc5e5
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2018-10-22 16:09:45 +0300

    Flag bad requests

--- routes/upload.js
+++ routes/upload.js
@@ -4,26 +4,29 @@ const multer = require('multer');
 const UPLOAD_PATH = 'tmp/uploads/';
 const path = require('path');
 
-const upload = multer(
-    {
-      dest: UPLOAD_PATH,
-      fileFilter: function(req, file, cb) {
-        const filetypes = /wave|wav/;
-        const mimetype = filetypes.test(file.mimetype);
-        const extname = filetypes.test(
-            path.extname(file.originalname).toLowerCase());
-        cb(null, (mimetype && extname));
-      },
+const upload = multer({
+  dest: UPLOAD_PATH,
+  fileFilter: function(req, file, cb) {
+    const filetypes = /wave|wav/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(
+        path.extname(file.originalname).toLowerCase());
+    if (mimetype && extname) {
+      cb(undefined, true);
+    } else {
+      req.badFileType = 'Only audio/wav files are supported.';
+      cb(undefined, false);
     }
-);
+  },
+});
 const fs = require('fs');
 const {promisify} = require('util');
 const clear = promisify(fs.unlink);
 
 router.post('/', upload.single('wave'), (req, res) => {
   const file = req.file;
-  if (!file) {
-    return res.status(415).send('Only audio/wav files are supported.');
+  if (req.badFileType) {
+    return res.status(415).end(req.badFileType);
   }
   clear(file.path);
   return res.sendStatus(200);
